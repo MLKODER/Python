4451cbea519c2f4e5e72c666a66ba2fa8a5cc4ba
MLKODER <72186520+MLKODER@users.noreply.github.com>
2020-10-02 13:02:50 +0530

    Update question1.py
    Improved Version of Code:
    
    Here I used : 
    Format of the code: 
    
    value_when_true if condition else value_when_false
    
    so in place of:
    value_when_true=print("The sum is " , d)
    if condition=c>1000
    value_when_false=print("The product is " , c)
    
    so, instead of 4 lines, in one line your code is reduced.
    
    This is not an advanced version, it is one way to write your code and can be used where the one-line statement is there for if & else condition.
    
    Hope you will like it.

--- Assignment_1/question1.py
+++ Assignment_1/question1.py
@@ -2,8 +2,4 @@ a = int(input("Please enter an integer:"))
 b = int(input("Please enter another integer:"))
 c = a * b
 d = a + b
-if c>1000:
-    print("The sum is " , d)
-else: 
-    print("The product is " , c)
-   
+print("The sum is " , d) if c>1000 else print("The product is " , c)
